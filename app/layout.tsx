9c58c430db5d67afef73777bedcf53bf9c3eca05
isixe <isixe@outlook.com>
2025-04-26 16:07:45 +0800

    🎈 perf(app): add keywords, canonical and openGraph metadata for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,31 @@
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: 'LinkChecker',
-  description:
-    'Check all links on a website, identify broken links, and analyze different links.'
+import { headers } from 'next/headers'
+
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = await headers()
+  const protocol = headersList.get('x-forwarded-proto') || 'http'
+  const host = headersList.get('host') || 'localhost:3000'
+  const url = `${protocol}://${host}`
+
+  return {
+    title: 'LinkChecker',
+    keywords: 'check link, link check, link checker, broken link checker',
+    description:
+      'Check all links on a website, identify broken links, and analyze different links.',
+    alternates: {
+      canonical: url
+    },
+    openGraph: {
+      title: 'LinkChecker',
+      description:
+        'Check all links on a website, identify broken links, and analyze different links.',
+      url,
+      siteName: 'LinkChecker',
+      locale: 'en'
+    }
+  }
 }
 
 export default function RootLayout({
